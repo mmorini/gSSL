9d24e45569575febafc45ee118173aadc06ba291
Matteo Morini <matteo.morini@ens-lyon.fr>
2018-07-09 18:20:04 +0900

    Added (functional) test file for travis-ci.

---
+++ tests.py
@@ -0,0 +1,16 @@
+import subprocess
+import tempfile
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+
+
+def test():
+	_exec_notebook('Bridgeness_BiasedRandomWalk_pipeline.ipynb')
+
+
+
